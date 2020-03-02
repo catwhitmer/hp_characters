af335963185a27ee12962d0ffe20bc14babbbbca
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-02 15:07:57 +0000

    first commit

---
+++ lib/hp_characters.rb
@@ -0,0 +1,6 @@
+require "hp_characters/version"
+
+module HpCharacters
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hp_characters/version.rb
@@ -0,0 +1,3 @@
+module HpCharacters
+  VERSION = "0.1.0"
+end
