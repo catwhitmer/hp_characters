a4bd1047d044fa48a125ced1528cdb01be6cff84
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-03 00:06:51 +0000

    pseudo code 1

--- lib/hp_characters/cli.rb
+++ lib/hp_characters/cli.rb
@@ -1,7 +1,10 @@
 class HpCharacters::CLI
   
   def start 
-    puts "Hello"
+    puts "Welcome to the Harry Potter Character Library!"
+    puts "Which character would you like more information about?"
+    puts "Character numbers between 1-188."
+    puts "Type a number between 1-188 please! Each number represents a character."
   end
   
 end
--- lib/hp_characters/scraper.rb
+++ lib/hp_characters/scraper.rb
@@ -1,3 +1,5 @@
 class HpCharacters::CLI
   
+  
+  
 end
