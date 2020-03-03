543d8eabb92a4f31821e4e03d57d30adc0e2362a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-03 00:49:56 +0000

    pseudo code 2

--- lib/hp_characters/cli.rb
+++ lib/hp_characters/cli.rb
@@ -5,6 +5,21 @@ class HpCharacters::CLI
     puts "Which character would you like more information about?"
     puts "Character numbers between 1-188."
     puts "Type a number between 1-188 please! Each number represents a character."
-  end
+    input = gets.strip
+    case input
+      when 1 
+        puts ""
+      when 2 
+        puts ""
+      when 3 
+        puts ""
+      when 4
+        puts ""
+      when exit
+        puts "Goodbye"
+      else
+        puts "invalid choice"
+     end
+   end
   
 end
